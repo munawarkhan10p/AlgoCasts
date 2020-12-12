376716a493be55d20aa5e43f62a5c5d68748348e
munawarkhan10p <munawarkhan6656@gmail.com>
2020-12-12 23:40:06 +0500

    reverse string second and third solution

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,8 +7,23 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-return str.split('').reverse().join('');
+ 
+  return str.split('').reduce( (rev, char) => rev = char + rev,'')
 }
 
 module.exports = reverse;
  
+
+
+ // Solution 1
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
+
+// Solution 2
+// function reverse(str) {
+//     let reversed = '';
+//     for (let character of str)
+//         reversed = character + reversed;
+//     return reversed;
+// }
