e9750d08ade322fc7a1dcb4e168b21896241bf0b
munawarkhan10p <munawarkhan6656@gmail.com>
2020-12-13 17:08:10 +0500

    ny solution for palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,8 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    return  str.split('').reverse().join('') === str ? true : false;
+}
 
 module.exports = palindrome;
