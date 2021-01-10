d0010c0ff509ea492b18d96a60b9550ee764a115
munawarkhan10p <munawarkhan6656@gmail.com>
2021-01-10 09:02:51 +0500

    remove duplicates using key object and Object.keys

--- exercises/removeDuplicate/index.js
+++ exercises/removeDuplicate/index.js
@@ -3,19 +3,17 @@
 // https://dev.to/will_devs/javascript-how-to-remove-duplicate-values-from-arrays-lf0
 
 
-let originalArray = [1, 2, 3, 4, 1, 2, 3, 4]
+const originalArray = [1, 2, 3, 4, 1, 2, 3, 4];
 
-let uniqueArray = originalArray.filter((item, index, array) => {
-  return array.indexOf(item) === index
-})
+let key = {};
 
-// uniqueArray === [1, 2, 3, 4]
+const uniqueArray = originalArray.filter((item) => {
+  if (key[item]) return false;
 
-//The basic strategy here is to iterate through originalArray and check to see if the index of 
-//the item we are currently examining is the same as the index of the item in the originalArray.
+  key[item] = true;
+  return true;
+});
 
-//Because indexOf returns the first index that it finds for a given value, if it isn't 
-//a duplicate value then the index for that item must be the same!
 
-//Note that this method is not the most efficient: it executes in quadratic time. 
-//So if the arrays you're checking are very large in size, you may want to use a different method.
+console.log(Object.keys(key));
+  
